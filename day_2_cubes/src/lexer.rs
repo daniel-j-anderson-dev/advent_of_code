f8ce3781aa06cd7df2d3760a1f9ab9f0baffb315
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-12-04 02:05:40 -0500

    Enumerate Tokens and find them

--- day_2_cubes/src/lexer.rs
+++ day_2_cubes/src/lexer.rs
@@ -1,3 +1,55 @@
+#[derive(Debug)]
+pub enum Token {
+    Game(usize),
+    Red(usize),
+    Green(usize),
+    Blue(usize),
+    Other(String),
+}
+impl Token {
+    pub fn from_raw(raw_token: &[char]) -> Option<Self> {
+        if raw_token.starts_with(&['G', 'a', 'm', 'e']) {
+            let slice = raw_token[4..].iter().collect::<String>().trim().to_owned();
+            if let Ok(value) = slice.parse() {
+                return Some(Token::Game(value));
+            }
+        }
+        if raw_token.ends_with(&['r', 'e', 'd']) {
+            let slice = raw_token[..raw_token.len() - 3].iter().collect::<String>().trim().to_owned();
+            if let Ok(value) = slice.parse() {
+                return Some(Token::Red(value));
+            }
+        }
+        if raw_token.ends_with(&['g','r','e','e','n']) {
+            let slice = raw_token[..raw_token.len() - 5].iter().collect::<String>().trim().to_owned();
+            if let Ok(value) = slice.parse() {
+                return Some(Token::Green(value));
+            }
+        }
+        if raw_token.ends_with(&['b','l','u','e']) {
+            let slice = raw_token[..raw_token.len() - 4].iter().collect::<String>().trim().to_owned();
+            if let Ok(value) = slice.parse() {
+                return Some(Token::Blue(value));
+            }
+
+        }
+
+        Some(Token::Other(raw_token.iter().collect()))
+    }
+}
+impl std::fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", match self {
+            Self::Game(id) => format!("Game {}:", id),
+            Self::Red(val) => format!("{} red", val),
+            Self::Blue(val) => format!("{} blue", val),
+            Self::Green(val) => format!("{} green", val),
+            Self::Other(s) => s.to_string(),
+        })
+    }
+}
+
+/// an iterator over tokens in a slice of chars
 pub struct Lexer<'a> {
     content: &'a [char]
 }
@@ -23,46 +75,6 @@ impl <'a> Lexer<'a> {
         token_end
     }
 
-    /// returns the start/end of a alphabetic token representing a digit
-    fn find_digit_str_token(&self) -> Option<usize> {
-        if !self.content[0].is_alphabetic() {
-            return None;
-        }
-
-        let alpha_end = self.find_token_end(|character| character.is_alphabetic());
-        let alpha_token = &self.content[..alpha_end];
-
-        for i in 0..alpha_end {
-            let slice = &alpha_token[i..];
-    
-            if  slice.starts_with(&['r','e','d']) {
-                if i != 0 {
-                    return Some(i);
-                } else {
-                    return Some(3);
-                }
-            }
-            if  slice.starts_with(&['g','r','e','e','n']) {
-                if i != 0 {
-                    return Some(i);
-                } else {
-                    return Some(5);
-                }
-            }
-            if  slice.starts_with(&['G','a','m','e']) ||
-                slice.starts_with(&['b','l','u','e'])
-            {
-                if i != 0 {
-                    return Some(i);
-                } else {
-                    return Some(4);
-                }
-            }
-        }
-    
-        None
-    }
-
     /// This function truncates the left most token (ignoring whitespace) and returns it
     /// 
     /// 
@@ -72,7 +84,7 @@ impl <'a> Lexer<'a> {
         self.cut(self.find_token_end(token_definition))
     }
 
-    pub fn next_token(&mut self) -> Option<&'a [char]> {
+    pub fn next_token(&mut self) -> Option<Token> {
         // did we run out of tokens?
         if self.content.is_empty() {
             None
@@ -84,115 +96,84 @@ impl <'a> Lexer<'a> {
             self.next_token()
         }
 
-        // is the token a number. only do single digits
+        // is the token a number.
         else if self.content[0].is_numeric() {
-            Some(self.cut_token(|character| character.is_numeric()))
+            let number = self.cut_token(|character| character.is_numeric())
+                .iter()
+                .collect::<String>()
+                .trim()
+                .parse::<usize>()
+                .expect("only numeric chars");
+
+            self.cut_token(|character| character.is_whitespace());
+
+            
+            let color = self.cut_token(|character| character.is_alphabetic())
+                .iter()
+                .collect::<String>();
+            
+            let token = match color.as_str() {
+                "red" => Token::Red(number),
+                "green" => Token::Green(number),
+                "blue" => Token::Blue(number),
+                _ => Token::Other(format!("{} {}", number, color)),
+            };
+
+            Some(token)
         }
 
         // is the token a string
         else if self.content[0].is_alphabetic() {
-            if let Some(token_end) = self.find_digit_str_token() {
-                Some(self.cut(token_end))
+            let game = self.cut_token(|character| character.is_alphabetic()).iter().collect::<String>();
+
+            self.cut_token(|character| character.is_whitespace());
+
+            let id = self.cut_token(|character| character.is_numeric())
+                .iter()
+                .collect::<String>()
+                .trim()
+                .parse::<usize>()
+                .expect("only numeric characters");
+            
+            if game.as_str() == "Game" {
+                Some(Token::Game(id))
             }
             else {
-                self.cut_token(|character| character.is_alphabetic());
-                self.next_token()
+                Some(Token::Other(format!("{} {}", game, id)))
             }
         }
 
         else {
-            Some(self.cut(1))
+            let raw_token = self.cut(1).iter().collect::<String>();
+            Some(Token::Other(raw_token))
+            // self.next_token()
         }
     }
 }
 impl Into<Vec<String>> for Lexer<'_> {
     fn into(self) -> Vec<String> {
         self.map(|token| {
-            token.iter().collect()
+            token.to_string()
         })
         .collect()
     }
 }
 impl<'a> Iterator for Lexer<'a> {
-    type Item = &'a [char];
+    type Item = Token;
     fn next(&mut self) -> Option<Self::Item> {
         self.next_token()
     }
 }
 
 #[test]
-fn alpha_ending_in_num() {
-    let input = "xyzone
-abctwo
-abctwo4";
-
-    for line in input.lines() {
-        let line_chars = line.chars().collect::<Vec<_>>();
-        let tokens: Vec<String> = Lexer::new(&line_chars).into();
-        
-        print!("{}\n[", line);
-        for (i, token) in tokens.iter().enumerate() {
-            print!("{}{}", token, if i == tokens.len() - 1 {""} else {", "});
-        }
-        println!("]\n");
-    }
-}
-
-#[test]
 fn test() {
-    let input = "1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet";
-
+    let input = std::fs::read_to_string("../day2.txt").unwrap();
     for line in input.lines() {
-        let line_chars = line.chars().collect::<Vec<_>>();
-        let tokens: Vec<String> = Lexer::new(&line_chars).into();
+        let chars = line.chars().collect::<Vec<_>>();
+        let tokens = Lexer::new(&chars);
 
-        print!("{}\n[", line);
-        for (i, token) in tokens.iter().enumerate() {
-            print!("{}{}", token, if i == tokens.len() - 1 {""} else {", "});
+        for token in tokens {
+            println!("{:?}", token);
         }
-        println!("]\n");
     }
-}
-
-#[test]
-fn number_tokens() {
-    let input = "oneone
-twotwo
-threethree
-fourfour
-fivefive
-sixsix
-sevenseven
-eighteight
-ninenine".chars().collect::<Vec<_>>();
-
-    let tokens: Vec<String> = Lexer::new(&input).into();
-
-    print!("{}\n[", input.iter().collect::<String>());
-    for (i, token) in tokens.iter().enumerate() {
-        print!("{}{}", token, if i == tokens.len() - 1 {""} else {", "});
-    }
-    println!("]\n");
-
-    assert_eq!("one",   tokens[ 0]);
-    assert_eq!("one",   tokens[ 1]);
-    assert_eq!("two",   tokens[ 2]);
-    assert_eq!("two",   tokens[ 3]);
-    assert_eq!("three", tokens[ 4]);
-    assert_eq!("three", tokens[ 5]);
-    assert_eq!("four",  tokens[ 6]);
-    assert_eq!("four",  tokens[ 7]);
-    assert_eq!("five",  tokens[ 8]);
-    assert_eq!("five",  tokens[ 9]);
-    assert_eq!("six",   tokens[10]);
-    assert_eq!("six",   tokens[11]);
-    assert_eq!("seven", tokens[12]);
-    assert_eq!("seven", tokens[13]);
-    assert_eq!("eight", tokens[14]);
-    assert_eq!("eight", tokens[15]);
-    assert_eq!("nine",  tokens[16]);
-    assert_eq!("nine",  tokens[17]);
 }

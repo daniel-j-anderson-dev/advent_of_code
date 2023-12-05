e6618569ecf67b8d7a75cadb74c6014cf9c05ecf
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-12-05 00:47:58 -0500

    Solve day 2 part 1

--- day_2_cubes/src/main.rs
+++ day_2_cubes/src/main.rs
@@ -1,21 +1,33 @@
 mod lexer;
 mod game;
 
-use crate::{
-    game::Game,
-    lexer::Lexer,
-};
+use crate::game::Game;
 
 fn main() {
     let input = std::fs::read_to_string("day2.txt")
         .expect("Failed to open day 2 input");
 
+    let mut sum = 0;
+
     for line in input.lines() {
-        let line_chars = line.chars().collect::<Vec<_>>();
-        let line_tokens = Lexer::new(&line_chars).collect::<Vec<_>>();
-
-        let Ok(game) = line.parse::<Game>() else { continue }; // each game is missing the last CubeSet
-
-        println!("tokens: {:?}\nparsed: {:?}\n", line_tokens, game);
+        
+        let game = match line.parse::<Game>() {
+            Ok(game) => game,
+            Err(game_parse_error) => {
+                eprintln!("Invalid game: {}", game_parse_error);
+                continue;
+            },
+        };
+        
+        if game.is_possible() {
+            sum += game.id();
+            print!("  POSSIBLE ");
+        } else {
+            print!("IMPOSSIBLE ");
+        }
+        
+        println!("{}", line);
     }
+
+    println!("{}", sum);
 }

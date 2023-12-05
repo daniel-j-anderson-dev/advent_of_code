3261d4cebc02c67d41908e411c9c8c344b033eac
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-12-05 14:41:58 -0500

    parse day 3 input

--- day_3_gear/src/main.rs
+++ day_3_gear/src/main.rs
@@ -1,3 +1,13 @@
+use grid::Grid;
+
+mod grid;
+
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("day3.txt")
+        .expect("Failed to open day 3 input");
+
+    let grid: Grid<140, 140> = input.parse()
+        .expect("failed to parse day 3 input");
+
+    println!("{:#?}", grid);
 }

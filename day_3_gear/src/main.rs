35d9262742d5e68bfc04bbef28d2a9bcf9b29bb6
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-12-07 12:59:46 -0500

    test str lexer

--- day_3_gear/src/main.rs
+++ day_3_gear/src/main.rs
@@ -1,29 +1,18 @@
-use grid::{Grid, TokenKind};
-
+mod lexer;
 mod grid;
 
-fn main() {
-    let input = std::fs::read_to_string("day3.txt")
-        .expect("Failed to open day 3 input");
+use grid::{Grid, TokenKind};
+use lexer::Lexer;
 
-    let grid: Grid<140, 140> = input.parse()
-        .expect("failed to parse day 3 input");
-    let mut part_numbers = Vec::new();
 
-    let number_started = false;
+fn main() -> Result<(), std::io::Error> {
+    let input = std::fs::read_to_string("day3.txt")?;
+    let lexer = Lexer::new(&input);
 
-    for (row_index, row) in grid.rows().iter().enumerate() {
-        for (col_index, token) in row.iter().enumerate() {
-            
-            if token.kind().is_number()  {
-                
-                if grid.is_token_a_part_number(row_index, col_index) {
-                    part_numbers.push(token);
-                }
-            }
-        }
+    for token in lexer {
+        println!("{}", token);
     }
 
-    println!("{:#?}", part_numbers);
+    Ok(())
 }
 

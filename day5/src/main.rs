be714ee7118217711941761497b4ac483bf9d838
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-04-19 09:50:27 -0400

    moved test to main

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,8 +1,30 @@
-pub mod parser;
+use pest::Parser;
+
 pub mod ast;
+pub mod parser;
+use crate::{
+    ast::Almanac,
+    parser::{AlmanacParser, Rule},
+};
 
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
     println!("Hello, world!");
+}
+
+#[test]
+fn print_parse_output() {
+    let file = AlmanacParser::parse(Rule::File, INPUT).unwrap();
+    for pair in file {
+        println!("{:#?}", pair);
+    }
+}
+
+#[test]
+fn ast() {
+    let almanac = INPUT.parse::<Almanac>().unwrap();
+    for node in almanac.iter() {
+        println!("{}", node);
+    }
 }
--- day5/src/parser.rs
+++ day5/src/parser.rs
@@ -1,22 +1,9 @@
 use pest::Parser;
+
 use pest_derive::Parser;
 
-use crate::INPUT;
+use crate::{ast::Almanac, INPUT};
 
 #[derive(Debug, Parser)]
 #[grammar = "../almanac.pest"]
 pub struct AlmanacParser;
-
-#[test]
-fn f() {
-    let file = AlmanacParser::parse(Rule::File, INPUT);
-
-    if let Err(ref e) = file {
-        println!("{}\n{:#?}", e, e);
-    }
-
-    let file = file.unwrap();
-    for pair in file {
-        println!("{:#?}", pair);
-    }
-}
